7131cf6ff5c3f13660cb2da682163ad9b58e7206
Sue Roh <sr3022@columbia.edu>
2016-11-18 00:28:41 -0500

    Added drawer

--- browser/components/navbar/Navbar.js
+++ browser/components/navbar/Navbar.js
@@ -7,26 +7,56 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import {Toolbar, ToolbarGroup, ToolbarTitle} from 'material-ui';
 import FontIcon from 'material-ui/FontIcon';
 import FlatButton from 'material-ui/FlatButton';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 import { white } from 'material-ui/styles/colors';
 
 // Material CSS rules
 const toolbarText = {color: white, padding: 0}
 const centerText = {marginLeft: '5%'}
 
-export default (props) => (
-  <Toolbar id="navbar" style={{backgroundColor: '#2b4b91'}}>
-    <ToolbarGroup>
-      <div className="navbar-item">
-        <Link to="/">
-          <FontIcon className="material-icons" color={white} hoverColor="#00BCD4">home</FontIcon>
-        </Link>
-      </div>
-    </ToolbarGroup>
-    <ToolbarGroup style={centerText}>
-      <Link to="/">
-        <ToolbarTitle style={toolbarText} text="MusoLingo" />
-      </Link>
-    </ToolbarGroup>
-    <NavbarMenu { ...props } />
-  </Toolbar>
-)
+export default class Navbar extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      open: false
+    }
+
+    this.handleToggle = this.handleToggle.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleToggle() {
+    this.setState({open: !this.state.open});
+  }
+
+  handleClose() {
+    this.setState({open: false});
+  }
+
+  render(props) {
+    return (
+      <Toolbar id="navbar" style={{backgroundColor: '#2b4b91'}}>
+        <ToolbarGroup>
+          <div className="navbar-item">
+            <i id="drawer-icon" className="material-icons" hoverColor="#00BCD4" onClick={this.handleToggle}>view_headline</i>
+            <Drawer 
+              docked={false}
+              width={200}
+              onRequestChange={(open) => this.setState({open})}
+              open={this.state.open}>
+              <MenuItem onClick={this.handleClose}>Lesson 1</MenuItem>
+              <MenuItem onClick={this.handleClose}>Lesson 2</MenuItem>
+            </Drawer>
+          </div>
+        </ToolbarGroup>
+        <ToolbarGroup style={centerText}>
+          <Link to="/">
+            <ToolbarTitle style={toolbarText} text="MusoLingo" />
+          </Link>
+        </ToolbarGroup>
+        <NavbarMenu { ...props } />
+      </Toolbar>
+    )
+  }
+}
