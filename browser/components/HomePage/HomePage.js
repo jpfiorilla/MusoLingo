f738ecd851a85caa20aa8ea8d12642dd639cf01b
Sue Roh <sr3022@columbia.edu>
2016-11-18 15:48:45 -0500

    Renamed Homepage and added stylesheets folder

--- browser/components/HomePage/HomePage.js
+++ browser/components/HomePage/HomePage.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default class HomePage extends React.Component {
     render() {
         return (
-            
+            <div className="homepage-1">
+                <img id="homepage-image" src="/homepage-image.jpg" />
+            </div>
         )
     }
 }
