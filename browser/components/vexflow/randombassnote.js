73cb1ac038b5be28d58dfcd1a524733573875cd9
audiosprite <jpfiorilla@gmail.com>
2016-11-24 14:26:14 -0500

    bass clef quiz

---
+++ browser/components/vexflow/randombassnote.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import { getNoteName, randomTrebleNoteName } from '../../utils';
+
+import Vex from 'vexflow';
+
+export default class RandomBassNote extends Component {
+    render(){
+        return (
+        <div>
+            <div id="staff"></div>
+        </div>
+        )
+    }
+    componentDidMount(){
+        const {note} = this.props;
+
+        let VF = Vex.Flow;
+
+        // Create an SVG renderer and attach it to the DIV element named "boo".
+        var div = document.getElementById("staff")
+        var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+
+        // Configure the rendering context.
+        renderer.resize(500, 200);
+        var context = renderer.getContext();
+        context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+
+        // Create a stave of width 400 at position 10, 40 on the canvas.
+        var stave = new VF.Stave(10, 40, 400);
+
+        // Add a clef and time signature.
+        // stave.addClef("treble").addTimeSignature("4/4");
+        stave.addClef("bass");
+
+        // Connect it to the rendering context and draw!
+        stave.setContext(context).draw();
+
+        if (getNoteName(note)[1]){
+        var notes = [
+            new VF.StaveNote({clef: "bass", keys: [note], duration: "w" }).addAccidental(0, new VF.Accidental(getNoteName(note)[1]))
+        ];} else {
+            notes = [
+            new VF.StaveNote({clef: "bass", keys: [note], duration: "w" })
+            ];
+        }
+
+        // Create a voice in 4/4 and add above notes
+        var voice = new VF.Voice({num_beats: 4,  beat_value: 4});
+        voice.addTickables(notes);
+
+        // Format and justify the notes to 400 pixels.
+        var formatter = new VF.Formatter().joinVoices([voice]).format([voice], 400);
+
+        // Render voice
+        voice.draw(context, stave);
+    }
+    
+}
--- db/Seed_Helper/Quiz_Seed.js
+++ db/Seed_Helper/Quiz_Seed.js
@@ -2,6 +2,10 @@ const quiz = [{
     lesson_id: 1,
     title: "Guess the Note Name",
     question_types: ['guessNoteName', 'guessNoteName', 'guessNoteName', 'guessNoteName', 'guessNoteName', 'guessNoteName', 'guessNoteName', 'guessNoteName', 'guessNoteName', 'guessNoteName']
+}, {
+    lesson_id: 4,
+    title: "Guess the Note Name",
+    question_types: ['guessBassNoteName', 'guessBassNoteName', 'guessBassNoteName', 'guessBassNoteName', 'guessBassNoteName', 'guessBassNoteName', 'guessBassNoteName', 'guessBassNoteName', 'guessBassNoteName', 'guessBassNoteName']
 }];
 
 module.exports = quiz;
