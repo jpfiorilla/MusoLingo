101711f3f96052b42961cc0bab7365d0158b1a20
mithunder <mselvaratnam@gmail.com>
2016-11-28 16:28:10 -0500

    entered most of the text for lesson 2

--- db/Seed_Helper/Lesson_Seed.js
+++ db/Seed_Helper/Lesson_Seed.js
@@ -4,7 +4,9 @@ var lessons = [{"title":"Notes","level":1,"description":"Learn how to read rhyth
 {"title":"Staves","level":2,"description":"Learn how to understand the staff.","topic_id":1},
 {"title":"Time Signatures","level":3,"description":"Learn how to read time.","topic_id":1},
 {"title":"Bass clef","level":4,"description":"Learn how to read bass clef.","topic_id":1},
-{"title":"Intervals","level":1,"description":"Learn how to count intervals.","topic_id":2}
+{"title":"Octaves & Half-Steps","level":1,"description":"Learn about octaves.","topic_id":2},
+{"title":"Distance","level":2,"description":"Part 1 of understanding intervals.","topic_id":2},
+{"title":"Quality","level":3,"description":"Part 2 of understanding intervals.","topic_id":2},
 ];
 
 
--- db/Seed_Helper/Slide_Seed.js
+++ db/Seed_Helper/Slide_Seed.js
@@ -258,23 +258,314 @@ lessononefour.forEach((slide, idx) => {
   slide.number = idx + 1;
 });
 
-lessontwoone = [{
-  "title": "Intervals - semitones",
+lessontwoone = [
+   {
+  "title": "1",
+  "number": 1,
   "slideContent": [
     {
-      text: "An interval is defined as the space between two notes."
+      text: "We learned that there are 12 notes from A to G#, including the accidentals."
     }, {
-      img: ''
-    }, {
-      text: ""
+      text: "But a piano has much more than just 12 keys. So what notes do all those other keys correspond to?"
     }
-  ]
-}]
+  ],
+  "lesson_id": 5
+}, {
+  "title": "2",
+  "number": 2,
+  "slideContent": [
+    {
+      text: "You may have noticed that all keyboards have the same repeating pattern of 12 keys. Each of these sets of 12 corresponds to a single octave."
+    },
+    {
+      img: '' // insert image of piano octaves
+    },
+    {
+      text: "Each octave contains the 12 notes from A to G#. However, the sounds produced aren't exactly the same. As we move right along the keyboard, we are ascending octaves, and the notes have a higher pitch."
+    }
+  ],
+  "lesson_id": 5
+}, {
+  "title": "3",
+  "number": 3,
+  "slideContent": [
+    {
+      text: "A note's letter name tells us which note it is, but it doesn't tell us which octave that note is in."
+    },
+    {
+      text: "Fortunately, a musical staff does."
+    },
+    // {
+    //   vex: {
+    //     type: '',
+    //     num: '',
+    //     info: [],
+    //   }
+    // },
+    {
+      text: "Both of these notes represent C. But the one on the left represents C in the fourth octave (C3), while the one on the right represents C in the fifth octave (C4)." // add sound sample that plays both C notes?
+    }
+  ],
+  "lesson_id": 5,
+}, {
+  "title": "4",
+  "number": 4,
+  "slideContent": [
+    {
+      text: "If you were playing on a full 88-key piano, the lowest note would be A0. C4 would be the fourth C key moving from the left."
+    },
+    {
+      img: '' // insert image of C4/middle C
+    },
+    {
+      text: "On a smaller keyboard, like many MIDI keyboards, the notes may be mapped out differently, and the starting octave can usually be adjusted with a button. Our on-screen keyboard starts with C3 and ends at B4"
+    }
+  ],
+  "lesson_id": 5
+}, {
+  "title": "5",
+  "number": 5,
+  "slideContent": [
+    {
+      text: "A half-step, or 'semitone,' describes the distance between two consecutive notes. Two consecutive half-steps in a row may also be called a whole-step."
+    },
+    {
+      vex: '' // show a half step and a whole step
+    },
+    {
+      text: "There is one half-step between F and Gb. There is one whole step between F and G. And there are four half-steps, or two whole-steps, between G and A. And so on..."
+    }
+  ],
+  "lesson_id": 5
+}, {
+  "title": "6",
+  "number": 6,
+  "slideContent": [
+    {
+      text: "Half-steps and octaves will be very useful in understanding our next topic - intervals!"
+    }
+  ],
+  "lesson_id": 5
+}
+]
 lessontwoone.forEach((slide, idx) => {
   slide.lesson_id = 5;
   slide.number = idx + 1;
 });
 
-const slides = _.flatten([lessononeone, lessononetwo, lessononethree, lessononefour, lessontwoone]);
+lessontwotwo = [
+  {
+    "title": "1",
+    "slideContent": [
+      {
+        text: "In music theory, an interval describes the distance between two notes."
+      },
+      {
+        text: "There are two aspects to intervals: distance and quality. This lesson will cover distance."
+      }
+    ]
+  },
+  {
+    "title": "2",
+    "slideContent": [
+      // {
+      //   vex: { // c3 and d3
+      //
+      //   }
+      // },
+      {
+        text: "Let's start with C3 and D3. In terms of distance, the interval between these two notes will always be a second, regardless of any accidentals in either note."
+      },
+      {
+        text: "This goes for all consecutive letter names, as long as they're in the same octave."
+      }
+    ]
+  },
+  {
+    "title": "3",
+    "slideContent": [
+      {
+        vex: { // c3 and e3
+
+        }
+      },
+      {
+        text: "Just as any interval between C and D is a second, any interval between C and E is a third."
+      }
+    ]
+  },
+  {
+    "title": "4",
+    "slideContent": [
+      {
+        vex: {
+
+        }
+      },
+      {
+        text: "Are you starting to see a pattern? The interval between C and F is a fourth."
+      }
+    ]
+  },
+  {
+    "title": "5",
+    "slideContent": [
+      {
+        vex: {
+
+        }
+      },
+      {
+        text: "C and G form a fifth."
+      }
+    ]
+  },
+  {
+    "title": "6",
+    "slideContent": [
+      {
+        vex: {
+
+        }
+      },
+      {
+        text: "C and A form a sixth."
+      }
+    ]
+  },
+  {
+    "title": "7",
+    "slideContent": [
+      {
+        vex: {
+
+        }
+      },
+      {
+        text: "C and B form a seventh."
+      }
+    ]
+  },
+  {
+    "title": "8",
+    "slideContent": [
+      {
+        vex: {
+
+        }
+      },
+      {
+        text: "What interval do we get if we compare C with the C in the next octave? I just said it: an octave!"
+      }
+    ]
+  },
+  {
+    "title": "9",
+    "slideContent": [
+      {
+        vex: {
+
+        }
+      },
+      {
+        text: "Intervals can also be counted downward. For example, G2 can be described as being a fourth below C3. If we were to count upward however, they would form a fifth"
+      },
+      {
+        text: "Don't worry too much about counting downward. For our purposes, we'll mostly be counting upward."
+      }
+    ]
+  },
+  {
+    "title": "10",
+    "slideContent": [
+      {
+        text: "The quiz for this lesson will test your ability to identify intervals based on distance. Try to get used to how different intervals look on a staff. This skill will be invaluable as you continue your study of harmony."
+      }
+    ]
+  }
+]
+lessontwotwo.forEach((slide, idx) => {
+  slide.lesson_id = 6;
+  slide.number = idx + 1;
+});
+
+lessontwothree = [
+  {
+    "title": '',
+    "slideContent": [
+      {
+        text: "The distance aspect of intervals is pretty straightforward, right? It's just simple counting."
+      }
+    ]
+  },
+  {
+    "title": '',
+    "slideContent": [
+      {
+        vex: { // c3 and d3
+
+        }
+      },
+      {
+        text: "Let's take another look at C3 and D3. Remember when I said that the interval between these notes will always be a second, regardless of whether or not accidentals show up in either note?"
+      }
+    ]
+  },
+  {
+    "title": '',
+    "slideContent": [
+      {
+        vex: { // c3 and d3, another of c3 and Db3
+
+        }
+      },
+      {
+        text: "Both of these intervals are seconds, but they are NOT the same interval. If we were to play each of them, they would sound different."
+      }, // sound sample of both intervals?
+      {
+        text: "Even though these intervals have the same distance, they differ in <b>quality</b>. C and Db are consecutive notes, with a single <b>half-step</b> between them."
+      },
+      { // possible move to a new slide
+        text: "Their interval quality is <b>minor</b>, and the full name of the intervals is a <b>minor second</b>."
+      }
+    ]
+  },
+  {
+    "title": '',
+    "slideContent": [
+      {
+        vex: { // C and Eb; C and E
+
+        }
+      },
+      {
+        text: "Since we have C and some variation of E in both of these examples, we know that both intervals are <b>thirds</b>."
+      },
+      {
+        text: "There are 3 half-steps between C and Eb, so they form a <b>minor third</b>. There are 4 half-steps between C and E, so they form a <b>major third</b>."
+      }
+    ]
+  },
+  {
+    "title": '',
+    "slideContent": [
+      {
+        text: "Starting to see the pattern? The quality of the interval is determined by the number of half-steps between the two notes."
+      },
+      {
+        img: ''// table comparing # of semitones to interval name
+      },
+      {
+        text: "The terminology changes once we get to fourths and fifths, but the concepts are the same."
+      }
+    ]
+  },
+]
+lessontwothree.forEach((slide, idx) => {
+  slide.lesson_id = 7;
+  slide.number = idx + 1;
+});
+
+const slides = _.flatten([lessononeone, lessononetwo, lessononethree, lessononefour, lessontwoone, lessontwotwo, lessontwothree]);
 
 module.exports = slides;
