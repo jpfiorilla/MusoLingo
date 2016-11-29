31bbeb6ba46440020f4e10ccdafeb89f2f54fb2b
Sue Roh <sr3022@columbia.edu>
2016-11-29 14:20:45 -0500

    Added draggable onscreen keyboard

--- browser/components/Keyboard/Keyboard.js
+++ browser/components/Keyboard/Keyboard.js
@@ -2,9 +2,8 @@ import React from 'react';
 import { polySynth } from '../../instruments';
 import { mapSoundsToPiano, mapSoundsToComputerKeyboard, selectKeysOnDOM, toggleKeyboardDisplay, mapSoundsToComputerKeyboardForGame } from '../../onScreenKeyboard';
 
-
 export default class Keyboard extends React.Component {
-
+  
   componentDidMount(){
     piano(document.querySelector("#keyboard"), {range: {startKey: "C", startOctave: 3, endKey: "B", endOctave: 4}});
 
@@ -13,6 +12,8 @@ export default class Keyboard extends React.Component {
     mapSoundsToComputerKeyboard();
     // mapSoundsToComputerKeyboardForGame();
     toggleKeyboardDisplay();
+
+    $("#keyboard").draggable();
   }
 
   render(){
@@ -22,7 +23,11 @@ export default class Keyboard extends React.Component {
           <img id="showKeyboard-icon" src="/images/piano-icon.png" />
           <div id="toggleKeyboard-text">Show</div>
         </div>
-        <div id="keyboard" style={{display: 'none'}}></div>
+        <div id="keyboard" className="ui-widget-content" style={{display: 'none'}}>
+          <div id="keyboard-help">?
+            <div id="keyboard-help-text">Click and drag me to reposition</div>
+          </div>
+        </div>
       </div>
     )
   }
