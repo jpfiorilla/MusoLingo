eced9705be126c3dbb53600fa9b3d68c250200f2
Brian Long <blong8334@gmail.com>
2016-12-03 17:42:13 -0500

    Made a slide seed guide file

--- db/Seed_Helper/Slide_Seed.js
+++ db/Seed_Helper/Slide_Seed.js
@@ -247,44 +247,6 @@ var lessononeone = [{
     }
   ]
 }];
-// let lessononeone = [{
-//   "title": "Basic rhythmic notation",
-//   "slideContent": [
-//     {
-//       "text": "Welcome to <b>88 Keys</b>!"
-//     },
-//     {
-//       'img': "http://i.imgur.com/PvRcW43.png"
-//     },
-//     {
-//       'externalRef': {
-//         'link': ["http://www.pgatour.com/"],
-//         'linkDisplay': ['Check This Out'],
-//         'description': ['Learn more here']
-//       }
-//     },
-//     {
-//       'pianoUserInput': {
-//         'notesToPlay': ['A3', 'D3']
-//       }
-//     },
-//     {
-//       'audioLink': {
-//         'source': 'http://www.stephaniequinn.com/Music/Canon.mp3',
-//         'type': 'audio/mp3',
-//         'description': 'check out this cool song'
-//       }
-//     },
-//     {
-//       'userTextInput': {
-//         'correctAnswer': 'horse',
-//         'question': 'Type horse',
-//         'correctAnswerMessage': 'Good job',
-//         'incorrectAnswerMessage': 'Try again'
-//       }
-//     }
-//   ]
-// }];
 
 lessononeone.forEach((slide, idx) => {
   slide.lesson_id = 1;
